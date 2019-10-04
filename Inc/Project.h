5ee029316b77a521e24008dacc310665a0becb08
ForestMrVince <Forest_Mu@users.noreply.github.com>
2019-10-04 18:53:58 +0800

    Update Project.h

--- Inc/Project.h
+++ Inc/Project.h
@@ -5,7 +5,7 @@
 #include <vector>
 
 //工程头文件
-#include "Matrix.h"			//矩阵运算函数
-#include "Variables.h"		//全局变量定义
-#include "Solver.h"			//迭代运算函数
 #include "Define.h"			//变量类型定义
+#include "Variables.h"		//全局变量定义
+#include "Matrix.h"			//矩阵运算函数
+#include "Solver.h"			//迭代运算函数
