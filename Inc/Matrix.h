aa8763332aeb58d14f5d4c6702057c646f9c9395
ForestMrVince <Forest_Mu@users.noreply.github.com>
2019-10-07 22:30:22 +0800

    Update Matrix.h
    更新函数名

--- Inc/Matrix.h
+++ Inc/Matrix.h
@@ -5,3 +5,8 @@
 //矩阵运算函数声明
 bool Project_GetAMatrix(Matrix_typedef *Matrix);	//矩阵输入函数
 bool Project_ShowAMatrix(Matrix_typedef *Matrix);	//显示一个矩阵
+Matrix_typedef Project_MatrixInversion(Matrix_typedef Matrix);//求逆
+Matrix_typedef Project_MatrixTransposition(Matrix_typedef Matrix);//求转置
+Matrix_typedef Project_MatrixMultiplication(Matrix_typedef Matrix_front, Matrix_typedef Matrix_back);//矩阵相乘
+Matrix_typedef Project_MatrixMultipliedByNumber(double Number, Matrix_typedef Matrix);//数与矩阵相乘
+Matrix_typedef Project_MatrixPlusMatrix(Matrix_typedef Matrix_front, Matrix_typedef Matrix_back);//矩阵相加
