721d799cc8d11095ce9a144e4bc7b228df01a1b5
ForestMrVince <Forest_Mu@users.noreply.github.com>
2019-10-08 12:00:06 +0800

    Update Matrix.h
    更新了矩阵求逆函数的返回值和形式参数。

--- Inc/Matrix.h
+++ Inc/Matrix.h
@@ -5,7 +5,7 @@
 //矩阵运算函数声明
 bool Project_GetAMatrix(Matrix_typedef *Matrix);	//矩阵输入函数
 bool Project_ShowAMatrix(Matrix_typedef *Matrix);	//显示一个矩阵
-Matrix_typedef Project_MatrixInversion(Matrix_typedef Matrix);//求逆
+bool Project_MatrixInversion(Matrix_typedef Matrix, Matrix_typedef *MatrixReversed);//求逆
 Matrix_typedef Project_MatrixTransposition(Matrix_typedef Matrix);//求转置
 bool Project_MatrixMultiplication(Matrix_typedef Matrix_front, Matrix_typedef Matrix_back, Matrix_typedef *MatrixMultiplicated);//矩阵相乘
 Matrix_typedef Project_MatrixMultipliedByNumber(double Number, Matrix_typedef Matrix);//数与矩阵相乘
