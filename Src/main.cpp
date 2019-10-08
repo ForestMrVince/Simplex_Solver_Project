ac9d63a3db014ca17b18c01353b5901cd2c71144
ForestMrVince <Forest_Mu@users.noreply.github.com>
2019-10-08 10:28:46 +0800

    Update main.cpp
    更新了矩阵与数字相乘的函数。

--- Src/main.cpp
+++ Src/main.cpp
@@ -4,6 +4,8 @@ int main()
 {
 	Project_GetAMatrix(&Matrix);
 	Project_ShowAMatrix(&Matrix);
+	Matrix = Project_MatrixMultipliedByNumber(2,Matrix);
+	Project_ShowAMatrix(&Matrix);
 
 	std::cout << "hello world" << std::endl;
 
