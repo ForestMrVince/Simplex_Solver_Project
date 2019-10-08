bfc78d65e09984620fb08ba873f89f82c4bf94fc
ForestMrVince <Forest_Mu@users.noreply.github.com>
2019-10-08 23:01:13 +0800

    Update Matrix.h
    部分更新了矩阵求逆函数，以及行列式计算函数。

--- Inc/Matrix.h
+++ Inc/Matrix.h
@@ -10,3 +10,6 @@ Matrix_typedef Project_MatrixTransposition(Matrix_typedef Matrix);//求转置
 bool Project_MatrixMultiplication(Matrix_typedef Matrix_front, Matrix_typedef Matrix_back, Matrix_typedef *MatrixMultiplicated);//矩阵相乘
 Matrix_typedef Project_MatrixMultipliedByNumber(double Number, Matrix_typedef Matrix);//数与矩阵相乘
 bool Project_MatrixPlusMatrix(Matrix_typedef Matrix_front, Matrix_typedef Matrix_back, Matrix_typedef *MatrixAdded);//矩阵相加
+
+//行列式运算
+double Project_Determinant(Matrix_typedef Determinant);//行列式
