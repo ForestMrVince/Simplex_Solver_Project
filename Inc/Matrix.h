5646ba428d4a100bf326f408565d63434bf3a1c7
ForestMrVince <Forest_Mu@users.noreply.github.com>
2019-10-08 11:10:06 +0800

    Update Matrix.h
    更新了矩阵相加的矩阵运算函数。

--- Inc/Matrix.h
+++ Inc/Matrix.h
@@ -9,4 +9,4 @@ Matrix_typedef Project_MatrixInversion(Matrix_typedef Matrix);//求逆
 Matrix_typedef Project_MatrixTransposition(Matrix_typedef Matrix);//求转置
 Matrix_typedef Project_MatrixMultiplication(Matrix_typedef Matrix_front, Matrix_typedef Matrix_back);//矩阵相乘
 Matrix_typedef Project_MatrixMultipliedByNumber(double Number, Matrix_typedef Matrix);//数与矩阵相乘
-Matrix_typedef Project_MatrixPlusMatrix(Matrix_typedef Matrix_front, Matrix_typedef Matrix_back);//矩阵相加
+bool Project_MatrixPlusMatrix(Matrix_typedef Matrix_front, Matrix_typedef Matrix_back, Matrix_typedef *MatrixAdded);//矩阵相加
