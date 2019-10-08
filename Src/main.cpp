ec9e8abe1474a087748ebd3eee1d53f7763e0583
ForestMrVince <Forest_Mu@users.noreply.github.com>
2019-10-08 11:10:13 +0800

    Update main.cpp
    更新了矩阵相加的矩阵运算函数。

--- Src/main.cpp
+++ Src/main.cpp
@@ -4,9 +4,11 @@ int main()
 {
 	Project_GetAMatrix(&Matrix);
 	Project_ShowAMatrix(&Matrix);
-	Matrix = Project_MatrixMultipliedByNumber(2,Matrix);
+	Matrix = Project_MatrixMultipliedByNumber(1.5,Matrix);
 	Project_ShowAMatrix(&Matrix);
 	Matrix = Project_MatrixTransposition(Matrix);
+	Project_ShowAMatrix(&Matrix);
+	Project_MatrixPlusMatrix(Matrix, Matrix, &Matrix);
 	Project_ShowAMatrix(&Matrix);
 
 	std::cout << "hello world" << std::endl;
