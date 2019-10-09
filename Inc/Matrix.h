23701524830ad7d8dc47861b44eab7af79072a28
ForestMrVince <Forest_Mu@users.noreply.github.com>
2019-10-09 22:59:50 +0800

    Update Matrix.h
    更新完成了矩阵求逆的运算函数。

--- Inc/Matrix.h
+++ Inc/Matrix.h
@@ -10,6 +10,15 @@ Matrix_typedef Project_MatrixTransposition(Matrix_typedef Matrix);//求转置
 bool Project_MatrixMultiplication(Matrix_typedef Matrix_front, Matrix_typedef Matrix_back, Matrix_typedef *MatrixMultiplicated);//矩阵相乘
 Matrix_typedef Project_MatrixMultipliedByNumber(double Number, Matrix_typedef Matrix);//数与矩阵相乘
 bool Project_MatrixPlusMatrix(Matrix_typedef Matrix_front, Matrix_typedef Matrix_back, Matrix_typedef *MatrixAdded);//矩阵相加
+static Matrix_typedef AdjugateMatrix_fun(Matrix_typedef Matrix);//计算伴随矩阵;
 
 //行列式运算
 double Project_Determinant(Matrix_typedef Determinant);//行列式
+static void FullyArranged_fun(Matrix_typedef *FullyArranged, size_t n);//求行列式列序数排列组合矩阵
+static void FullyArranged_fun_Exhaustive(Matrix_typedef *FullyArranged, Matrix_Row FullyArrangedRowElement_temp, size_t start, size_t n, std::vector<size_t> *Line, std::vector<size_t> *factorial_vec);//求行列式列序数排列组合矩阵函数的穷举递归函数
+static double ReverseOrderNumber_fun(Matrix_Row FullyArranged_row);//求逆序数
+static void ReverseOrderNumber_fun_Exhaustive(double *Number, Matrix_Row Row);//求逆序数函数的穷举递归函数
+
+//test
+//void FullyArranged_fun(Matrix_typedef *FullyArranged, size_t n);
+//double ReverseOrderNumber_fun(Matrix_Row FullyArranged_row);//求逆序数
