378ee2e42677b00d4135628c3bf7f20e158d65bc
ForestMrVince <Forest_Mu@users.noreply.github.com>
2019-10-09 23:00:08 +0800

    Update Matrix.cpp
    更新完成了矩阵求逆的运算函数。

--- Src/Matrix.cpp
+++ Src/Matrix.cpp
@@ -117,13 +117,15 @@ bool Project_MatrixInversion(Matrix_typedef Matrix, Matrix_typedef *MatrixRevers
 {
 	if (Matrix.size() == Matrix.begin()->size())
 	{
-		if (Project_Determinant(Matrix) == 0)
+		double Determinant = Project_Determinant(Matrix);
+		//std::cout << Determinant << std::endl;//test
+		if (Determinant == 0)
 		{
 			return false;
 		}
 		else
 		{
-			;
+			*MatrixReversed = Project_MatrixMultipliedByNumber(1 / Determinant, AdjugateMatrix_fun(Matrix));
 		}
 	}
 	return false;
@@ -238,8 +240,167 @@ bool Project_MatrixPlusMatrix(Matrix_typedef Matrix_front, Matrix_typedef Matrix
 double Project_Determinant(Matrix_typedef Determinant)
 {
 	double result = 0;
+	size_t Determinant_n = Determinant.size(), Determinant_m = 1;
+	Matrix_Row FullyArrangedRow_temp(Determinant_n, 0);
+
+	for (size_t i = 0; i < Determinant_n; i++)
+	{
+		Determinant_m = Determinant_m * (Determinant_n - i);
+	}
+
+	Matrix_typedef FullyArranged(Determinant_m, FullyArrangedRow_temp);
+	
+	//Project_ShowAMatrix(&FullyArranged);//test
+
+	FullyArranged_fun(&FullyArranged, Determinant_n);
+
+	//Project_ShowAMatrix(&FullyArranged);//test
+	//std::cout << 3 << std::endl;//test
+	//std::cout << 3 << std::endl;//test
+
+	double DeterminantRowSum_temp = 1, j = 0;
+	size_t i = 0;
+
+	for (auto FullyArrangedRow_for : FullyArranged)
+	{
+		for (auto FullyArrangedElememt_for : FullyArrangedRow_for)
+		{
+			DeterminantRowSum_temp = DeterminantRowSum_temp * (((Determinant)[i])[FullyArrangedElememt_for-1]);
+			i++;
+		}
+
+		j = ReverseOrderNumber_fun(FullyArrangedRow_for);
+		//std::cout <<"DeterminantRowSum_temp = "<< DeterminantRowSum_temp << std::endl;//test
+		//std::cout << "j = " << j << std::endl;//test
+		result = result + DeterminantRowSum_temp * pow(-1, j);
+
+		i = 0;
+		DeterminantRowSum_temp = 1;
+	}
 
 
-
+	//std::cout << result << std::endl;//test
 	return result;
+}
+
+static void FullyArranged_fun(Matrix_typedef *FullyArranged, size_t n)
+{
+	Matrix_Row FullyArrangedRowElement_temp; double Elemet_temp = 0;
+
+	for (size_t i = 0; i < n; i++)
+	{
+		Elemet_temp = Elemet_temp + 1;
+		FullyArrangedRowElement_temp.push_back(Elemet_temp);
+	}
+
+	std::vector<size_t> Line(n, 0);
+	std::vector<size_t> factorial_vec;
+	FullyArranged_fun_Exhaustive(FullyArranged, FullyArrangedRowElement_temp, 0, n, &Line, &factorial_vec);
+}
+
+static void FullyArranged_fun_Exhaustive(Matrix_typedef *FullyArranged, Matrix_Row FullyArrangedRowElement_temp, size_t start, size_t n, std::vector<size_t> *Line, std::vector<size_t> *factorial_vec)
+{
+	Matrix_Row FullyArrangedRowElement_temp_temp, FullyArrangedRow_temp; size_t factorial = 1;
+	if (FullyArrangedRowElement_temp.size() != 0)
+	{
+		for (size_t i = 0; i < (FullyArrangedRowElement_temp.size() - 1); i++)
+		{
+			factorial = factorial * ((FullyArrangedRowElement_temp.size() - 1) - i);
+		}
+	}
+	if (factorial_vec->size() != n)
+	{
+		factorial_vec->push_back(factorial);
+	}
+
+	for (size_t i = 0; i < n - start; i++)
+	{
+		FullyArrangedRowElement_temp_temp = FullyArrangedRowElement_temp;
+		auto FullyArrangedRowElement_temp_temp_iterator = FullyArrangedRowElement_temp_temp.begin();
+		FullyArrangedRowElement_temp_temp.erase(FullyArrangedRowElement_temp_temp_iterator + i);
+		
+		(*Line)[start] = i;
+
+		for (size_t m = 0; m < factorial; m++)
+		{
+			size_t Line_base = 0;
+			Line_base = 0;
+			for (size_t j = 0; j < factorial_vec->size(); j++)
+			{
+				Line_base = Line_base + (((*factorial_vec)[j])*((*Line)[j]));
+				//std::cout << "j= " << j << std::endl;//test
+				//std::cout << "(*Line)[j]= " << (*Line)[j] << std::endl;//test
+				//std::cout << "(*factorial_vec)[j]= " << (*factorial_vec)[j] << std::endl;//test
+				//std::cout << "Line_base= " << Line_base << std::endl;//test
+			}
+
+			((*FullyArranged)[m + Line_base])[start] = FullyArrangedRowElement_temp[i];//需要注意每一层的那个数录入几次，比如列数为三的，第一层（start=0），每个数录入两次
+			//Project_ShowAMatrix(FullyArranged);//test
+			//std::cout << std::endl;//test
+			//std::cout << "i= " << i << std::endl;//test
+			//std::cout << "m= " << m << std::endl;//test
+			//std::cout << "factorial= " <<factorial << std::endl;//test
+			//std::cout << "start= " << start << std::endl;//test
+			//std::cout << std::endl;//test
+		}
+		FullyArranged_fun_Exhaustive(FullyArranged, FullyArrangedRowElement_temp_temp, start + 1, n, Line, factorial_vec);//m的问题
+	}
+	if (start != n)
+	{
+		(*Line)[start] = 0;
+	}
+}
+
+//double ReverseOrderNumber_fun(Matrix_Row FullyArranged_row)//test
+static double ReverseOrderNumber_fun(Matrix_Row FullyArranged_row)
+{
+	double ReverseOrderNumber = 0;
+
+	ReverseOrderNumber_fun_Exhaustive(&ReverseOrderNumber, FullyArranged_row);
+
+	return ReverseOrderNumber;
+}
+
+static void ReverseOrderNumber_fun_Exhaustive(double *Number, Matrix_Row Row)
+{
+	double RowFirestElement_now = Row[0];
+	Row.erase(Row.begin());
+	for (auto Element_temp : Row)
+	{
+		if (RowFirestElement_now > Element_temp)
+		{
+			(*Number)++;
+		}
+	}
+	if (Row.size() > 1)
+	{
+		ReverseOrderNumber_fun_Exhaustive(Number, Row);
+	}
+}
+
+static Matrix_typedef AdjugateMatrix_fun(Matrix_typedef Matrix)
+{
+	Matrix_Row AdjugateMatrixRow;
+	Matrix_typedef AdjugateMatrix, AlgebraicCofactor_M;
+
+	for (size_t i = 0; i < Matrix.size(); i++)
+	{
+		for (size_t j = 0; j < Matrix.size(); j++)
+		{
+			AlgebraicCofactor_M = Matrix;
+			auto AlgebraicCofactor_M_interator = AlgebraicCofactor_M.begin();
+			for (size_t k = 0; k < AlgebraicCofactor_M.size(); k++)
+			{
+				AlgebraicCofactor_M_interator->erase(AlgebraicCofactor_M_interator->begin()+j);
+				AlgebraicCofactor_M_interator = AlgebraicCofactor_M_interator + 1;
+			}
+			AlgebraicCofactor_M.erase(AlgebraicCofactor_M.begin() + i);
+
+			AdjugateMatrixRow.push_back(pow(-1,i+j)*Project_Determinant(AlgebraicCofactor_M));
+		}
+		AdjugateMatrix.push_back(AdjugateMatrixRow);
+		AdjugateMatrixRow.clear();
+	}
+
+	return AdjugateMatrix;
 }
